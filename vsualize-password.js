559545f138c3352b2e94b6386c7d4a5d39a84456
acender11 <153435162+acender11@users.noreply.github.com>
2023-12-11 14:23:45 +0300

    Update vsualize-password.js

--- vsualize-password.js
+++ vsualize-password.js
@@ -1,4 +1,4 @@
-function visualize_password() {
+function visualizePassword() {
     let image = document.querySelector("img");
     let input = document.querySelector("#password");
 
@@ -11,18 +11,33 @@ function visualize_password() {
     }
 }
 
-function validate_email() {
+function visualizePasswordAccept() {
+    let image = document.querySelector(".password-image-accept > img");
+    let input = document.querySelector("#password_accept");
+
+    if (image.getAttribute("src") === "/hidden.png") {
+        image.setAttribute("src", "/visible.png");
+        input.setAttribute("type", "text");
+    } else {
+        image.setAttribute("src", "/hidden.png");
+        input.setAttribute("type", "password");
+    }
+}
+
+function validateEmail() {
     let inputEmail = document.getElementById("email")
     let email = inputEmail.value;
 
     let validation = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
     let isValid = validation.test(email);
 
-    if (isValid) {
-        alert(inputEmail.style.borderBottomColor)
-        inputEmail.style.borderBottomColor = "#7fff00";
-        inputEmail.insertAdjacentHTML('afterend', '<div class="success">Получено успешно</div>')
-    } else {
-        alert("Адрес электронной почты введен неверно")
+    if (!document.querySelector(".success")) {
+        if (isValid) {
+            inputEmail.style.borderBottomColor = "rgb(6, 177, 20)";
+            inputEmail.insertAdjacentHTML('afterend', '<div class="success">Получено успешно</div>')
+            inputEmail.style.marginBottom = 0;
+        } else {
+            alert("Адрес электронной почты введен неверно")
+        }
     }
 }
